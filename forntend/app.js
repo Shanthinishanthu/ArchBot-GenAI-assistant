5f4e6a9463542bf331b3a51124daa903af7064c9
Shanthinishanthu <152088694+Shanthinishanthu@users.noreply.github.com>
2025-11-23 02:15:23 -0800

    Create app.js

---
+++ forntend/app.js
@@ -0,0 +1,9 @@
+console.log("Frontend loaded");
+
+// Simple function simulating user input and response
+function sendMessageToArchBot(message) {
+    console.log("User:", message);
+    console.log("ArchBot: Processing your request...");
+}
+
+sendMessageToArchBot("Show my account balance");
